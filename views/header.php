5d3508ee8c66ae73606b0215177540609bfcc9d1
Julio Cesar <vcjuliocesar@outlook.com>
2018-01-29 19:43:13 -0600

    cabecera

---
+++ views/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Sistema Web</title>
+    <meta charset="utf-8" />
+    <link href="<?php echo BASE_URL;?>templete/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
+    <link href="<?php echo BASE_URL;?>templete/bootstrap/css/bootstrap-responsive.min.css" rel="stylesheet" media="screen">
+    <link href="<?php echo BASE_URL;?>templete/assets/styles.css" rel="stylesheet" media="screen">
+    <link href="<?php echo BASE_URL;?>templete/assets/DT_bootstrap.css" rel="stylesheet" media="screen">
+          <!--[if lte IE 8]><script language="javascript" type="text/javascript" src="vendors/flot/excanvas.min.js"></script><![endif]-->
+          <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+          <!--[if lt IE 9]>
+              <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+          <![endif]-->
+  <style>
+   body
+   {
+    margin:0;
+    padding:0;
+    background-color:#f1f1f1;
+   }
+   .box
+   {
+    width:1270px;
+        padding:20px;
+    background-color:#fff;
+    border:1px solid #ccc;
+    border-radius:5px;
+    margin-top:25px;
+   }
+  </style>
+</head>
+<body>
+  <div class="container box">
